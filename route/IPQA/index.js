e35407b732fa8e9f7693882a4210d620ec70cd36
Garyhjj <379751452@qq.com>
2017-12-15 11:07:09 +0800

    improve

--- route/IPQA/index.js
+++ route/IPQA/index.js
@@ -10,7 +10,7 @@ var router = new Router({
 });
 
 router.get('/GetMachineCheckList', async(ctx) => {
-    let a = await request.get(config.proxy + ctx.request.url, reqOption(ctx));
+    let a = await request.get(config.proxy + ctx.request.url, reqOption(ctx)).catch(err =>err);
     if (isErr(a)) {
         ctx.response.status = a.statusCode || 400;
         ctx.response.body = a.error;
