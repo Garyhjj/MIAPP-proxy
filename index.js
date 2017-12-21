833502bd4ca98dc0a85cd8d66ed148f7b6a9930a
Garyhjj <379751452@qq.com>
2017-12-21 21:44:05 +0800

    improve script

--- index.js
+++ index.js
@@ -26,33 +26,36 @@ app.use(convert(cors()));
 app.use(koaBody());
 app.use(prepareReqOption);
 
-// 正常请求日志
-// app.use(logger({
-//     transports: [
-//         new(winston.transports.Console)({
-//             json: true,
-//             colorize: true
-//         }),
-//         new winston.transports.File({
-//             filename: `logs/success/${moment(new Date()).format('YYYYMMDD')}.log`
-//         })
-//     ]
-// }))
+if (process.env.NODE_ENV.trim() === 'production') {
+    // 正常请求日志
+    app.use(logger({
+        transports: [
+            new (winston.transports.Console)({
+                json: true,
+                colorize: true
+            }),
+            new winston.transports.File({
+                filename: `logs/success/${moment(new Date()).format('YYYYMMDD')}.log`
+            })
+        ]
+    }))
 
-// // 错误请求日志
-// app.use(logger({
-//     transports: [
-//         new winston.transports.Console({
-//             json: true,
-//             colorize: true
-//         }),
-//         new winston.transports.File({
-//             filename: `logs/error/${moment(new Date()).format('YYYYMMDD')}.log`
-//         })
-//     ]
-// }))
+    // 错误请求日志
+    app.use(logger({
+        transports: [
+            new winston.transports.Console({
+                json: true,
+                colorize: true
+            }),
+            new winston.transports.File({
+                filename: `logs/error/${moment(new Date()).format('YYYYMMDD')}.log`
+            })
+        ]
+    }))
 
-// app.use(handleError);
+    app.use(handleError);
+}
+
 
 route(app);
 
@@ -65,5 +68,5 @@ const options = {
     cert: fs.readFileSync('./ssl/server.pem')  //ssl文件路径
 };
 
-http.createServer(app.callback()).listen(80);
+http.createServer(app.callback()).listen(8000);
 https.createServer(options, app.callback()).listen(443);
